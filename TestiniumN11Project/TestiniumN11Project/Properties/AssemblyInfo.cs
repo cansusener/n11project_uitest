54c9557285d675704555ef530f23ba026b3cb5a5
cansusener <59277092+cansusener@users.noreply.github.com>
2020-02-16 01:56:22 +0300

    testinium
    testinium project upload

---
+++ TestiniumN11Project/TestiniumN11Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestiniumN11Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestiniumN11Project")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("446f69f1-fab2-4120-9c63-a10b51975cd2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestiniumN11Project/TestiniumN11Project/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestiniumN11Project
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.n11.com/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.LinkText("Giriş Yap")).Click();
+            driver.FindElement(By.Id("contentWrapper")).Click();
+            driver.FindElement(By.Id("email")).Clear();
+            driver.FindElement(By.Id("email")).SendKeys("testiniumN11@gmail.com");
+            driver.FindElement(By.Id("password")).Clear();
+            driver.FindElement(By.Id("password")).SendKeys("TestTest1234");
+            driver.FindElement(By.Id("loginButton")).Click();
+            driver.FindElement(By.Id("searchData")).Click();
+            driver.FindElement(By.Id("searchData")).Clear();
+            driver.FindElement(By.Id("searchData")).SendKeys("Bilgisayar");
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.XPath("//header[@id='header']/div/div/div[2]/div/a")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            var js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'})", driver.FindElement(By.ClassName("pagination")));
+            driver.FindElement(By.LinkText("2")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            js.ExecuteScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'})", driver.FindElement(By.ClassName("column")));
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.XPath("//div[@id='p-372507725']/div/a/h3")).Click();
+
+            driver.FindElement(By.LinkText("Sepete Ekle")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            var newPrice = Convert.ToDecimal(driver.FindElement(By.ClassName("newPrice")).Text.Split(' ')[0]);
+            driver.FindElement(By.ClassName("myBasket")).Click();
+            var priceArea = Convert.ToDecimal(driver.FindElement(By.ClassName("priceArea")).Text.Split(' ')[0]);
+
+            if (newPrice == priceArea)
+            {
+                driver.FindElement(By.XPath("//div[@id='newCheckout']/div/div/div[2]/div/section/table[2]/tbody/tr/td[3]/div/div/span")).Click();
+                driver.FindElement(By.XPath("//div[@id='newCheckout']/div/div/div[2]/div/section/table[2]/tbody/tr/td/div[3]/div[2]/span")).Click();
+
+            }
+            else
+            {
+                driver.FindElement(By.XPath("//div[@id='newCheckout']/div/div/div[2]/div/section/table[2]/tbody/tr/td/div[3]/div[2]/span")).Click();
+
+            }
+
+
+            //driver.FindElement(By.LinkText("Çıkış Yap")).Click();
+            driver.Quit();
+
+        }
+    }
+}
